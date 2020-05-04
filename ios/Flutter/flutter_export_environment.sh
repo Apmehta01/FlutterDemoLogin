cbce135c130a11b33ac11cbde714809a32525553
arpan mehta <arpanmehta.ap@gmail.com>
2020-05-04 17:16:08 +0530

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/maitri/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/maitri/Arpan/Demos/Flutter/Projects/flutter post screen example/Fluttr Login Screen Example/flutter_app_login_demo"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/maitri/flutter/bin/cache/artifacts/engine/ios"
